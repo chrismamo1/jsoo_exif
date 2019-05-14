317d2f16c2d21d6a1963f7eb2a2ab64eb7d44b6c
John Christopher McAlpine <christophermcalpine@gmail.com>
2019-05-14 16:48:14 +0200

    use lwt

--- jsoo_exif.mli
+++ jsoo_exif.mli
@@ -10,30 +10,30 @@ end*)
 module Img : sig
   type t
 
-  (** [img_of_string s callback] takes the textual data URI specified by [s] and
+  (** [img_of_string s] takes the textual data URI specified by [s] and
     * produces an image.
     *)
-  val of_data_uri : string -> (t -> 'a) -> unit
+  val of_data_uri : string -> t Lwt.t
   
-  (** [to_data_uri img callback] turns and image into a data URI. *)
-  val to_data_uri : t -> (string -> unit) -> unit
+  (** [to_data_uri img] turns and image into a data URI. *)
+  val to_data_uri : t -> string Lwt.t
   
   (** [to_file img] creates a [File] object representing the image. *)
-  val to_file : t -> (Js_of_ocaml.File.file Js_of_ocaml.Js.t -> 'a) -> unit
+  val to_file : t -> Js_of_ocaml.File.file Js_of_ocaml.Js.t Lwt.t
 
   (** [of_file file callback] Reads a file into an image. *)
-  val of_file : Js_of_ocaml.File.file -> (t -> 'a) -> unit
+  val of_file : Js_of_ocaml.File.file -> t Lwt.t
 end
 
 (** [get_data callback img tag] gets the EXIF tag [tag] from [img], passing it to
   * [callback].
   *)
-val get_data : Img.t -> string -> (string -> 'a) -> unit
+val get_data : Img.t -> string -> string Lwt.t
 
 (** [get_orientation callback img] is the same as [get_data callback img "Orientation"]
   *)
-val get_orientation : Img.t -> (string -> 'a) -> unit
+val get_orientation : Img.t -> string Lwt.t
 
 (** [reorient_image callback data_url] 
   *)
-val reorient_image : Img.t -> (Img.t -> 'a) -> unit
+val reorient_image : Img.t -> Img.t Lwt.t
